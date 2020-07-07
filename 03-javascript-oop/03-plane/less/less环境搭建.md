eb396b5e656b1703a4cb8d8932cc09cd89993171
Nicholas <nicholas@example.com>
2020-07-07 17:40:48 +0800

    忽略分享

---
+++ 03-javascript-oop/03-plane/less/less环境搭建.md
@@ -0,0 +1,34 @@
+
+安装nodejs
+# 默认C盘
+# 错误的观念 C盘装系统 D盘装软件 分区
+
+# 打开终端
+win10系统 win+R
+Win7系统 开始 命令行  
+cmd  
+
+# 检查是否安装成功
+node -v ==> 12.18.0
+npm -v ==> 6.xx
+
+# 错误提示
+node 不是不是内部或外部命令，也不是可运行的程序
+或批处理文件
+==> node环境安装出错
+
+vscode 集成终端
+默认 Windows PowerShell 切换成CMD
+ctrl+shift+P => 选择默认shell select default shell => CMD
+
+# 安装less
+npm install -g less
+==> + less@3.11.2
+
+
+# 测试less 是否安装成功
+lessc -v
+==> lessc 3.11.2 (Less Compiler) [JavaScript]
+
+# vscode 安装插件
+最左侧侧边栏 最后=>拓展 搜索 Easy LESS
---
+++ 07-less-sass-git-webpack/06/src/index.js
@@ -0,0 +1,29 @@
+import _ from "lodash";
+
+import "./style.less";
+
+import Icon from "./xxx.png";
+
+function component() {
+  var element = document.createElement("div");
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  element.classList.add("box");
+
+  var node = document.createElement("img");
+  node.src = Icon;
+
+  element.appendChild(node);
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+
+import printMe from './print.js';
+
+printMe()
